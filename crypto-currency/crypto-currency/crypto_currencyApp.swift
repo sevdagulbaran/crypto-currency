b5094061e1fb3a3aebedff3e99a01043394727bd
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2022-11-29 21:33:06 +0300

    🧚 project init

---
+++ crypto-currency/crypto-currency/crypto_currencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_currencyApp.swift
+//  crypto-currency
+//
+//  Created by Sevda Gul Baran on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct crypto_currencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
